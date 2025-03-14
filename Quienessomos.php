838eaf7fff89dc6dde43fd844bd882e194554843
AngelMontes2002 <angelmon.652@gmail.com>
2025-03-14 18:47:18 -0500

    Guardando cambios antes de hacer pull

---
+++ Quienessomos.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Contactos</title>
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/responsive.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="main-layout contact_page">
+   <header class="bg-dark py-3">
+      <div class="container d-flex justify-content-between align-items-center">
+         <a href="index.php"><img src="images/logo2.png" width="70px" height="50px" alt="Logo"></a>
+         <nav>
+            <ul class="nav">
+               <li class="nav-item"><a class="nav-link text-white" href="index.php">Home</a></li>
+               <li class="nav-item"><a class="nav-link text-white" href="Quienessomos.php">Quienes somos</a></li>
+               <li class="nav-item"><a class="nav-link text-white" href="contacto.php">Contactos</a></li>
+               <li class="nav-item"><a class="nav-link text-warning" href="login.php">Login</a></li>
+            </ul>
+         </nav>
+      </div>
+   </header>
+<!--  INFORMACION DE NOSOTROS -->
+      <section class="banner_main"style="http://localhost/Inventario_SaneyCORE/images/banner.png">
+         <div id="banner1" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#banner1" data-slide-to="0" class="active"></li>
+               <li data-target="#banner1" data-slide-to="1"></li>
+               <li data-target="#banner1" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="row">
+                           <div class="col-md-7">
+                              <div class="text-bg">
+                                 <h1> <span class="yellow"> Inventario</span> <br>De tus sueños</h1>
+                                 <p>It is a long established fact that a reader will be distracted by the readablecontent of a page when looking at its layout. The point of using Lorem </p>
+                                 <a class="read_more" href="contacto.php">Contacto</a><a class="read_more" href="#">M&aacute;s</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="row">
+                           <div class="col-md-7">
+                              <div class="text-bg">
+                                 <h1> <span class="yellow"> Siempre</span> <br>Contigo</h1>
+                                 <p>It is a long established fact that a reader will be distracted by the readablecontent of a page when looking at its layout. The point of using Lorem </p>
+                                 <a class="read_more" href="contacto.php">Contacto</a><a class="read_more" href="#">M&aacute;s</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="row">
+                           <div class="col-md-7">
+                              <div class="text-bg">
+                                 <h1> <span class="yellow"> Donde quieras</span> <br>Estaremos</h1>
+                                 <p>It is a long established fact that a reader will be distracted by the readablecontent of a page when looking at its layout. The point of using Lorem </p>
+                                 <a class="read_more" href="contacto.php">Contacto</a><a class="read_more" href="#">M&aacute;s</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#banner1" role="button" data-slide="prev">
+            <i class="fa fa-angle-left" aria-hidden="true"></i>
+            </a>
+            <a class="carousel-control-next" href="#banner1" role="button" data-slide="next">
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+         </div>
+      </section>
+      <div class="service">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2><img src="images/heading_icon.png" alt="#"/><span class="yellow">Todo Contigo</span> <br> <span class="appp_l">Comercial & Servicios </span></h2>
+                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="service_box">
+                     <i><img src="images/service_icon1.png" alt="#"/></i>
+                     <div class="service_text">
+                        <h3> Productos</h3>
+                        <p>It is a long established fact that a reader will be distracted by   the readable content of<br>
+                           a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it 
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <div class="service_box">
+                     <i><img src="images/service_icon2.png" alt="#"/></i>
+                     <div class="service_text">
+                        <h3>Los mejor a tu mano</h3>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of <br>
+                           a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it 
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <div class="service_box border_bottom1">
+                     <i><img src="images/service_icon4.png" alt="#"/></i>
+                     <div class="service_text">
+                        <h3>  Seguridas & Alertas </h3>
+                        <p>It is a long established fact that a reader will be distracted by the readable content ofa page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content he<br>
+                           re', making it 
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <a class="read_more" href="#">Inicio</a>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div id="about" class="about">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <div class="titlepage">
+                     <h2> <img src="images/heading_icon.png" alt="#">Conocenos</span></h2>
+                     <p>It is a long established fact that a reader will be distracted by the readable content ofa page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                  </div>
+               </div>
+               <div class="col-md-10 offset-md-1">
+                  <div class="about_img">
+                     <figure><img src="images/about_imglo.png" alt="#"/ style="margin: 0 310px;"></figure>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div id="project" class="project">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2><img src="images/heading_iconw.png" alt="#"/>Productos</h2>
+                     <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content  
+                     </p>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="project_box before_top">
+                     <figure><img src="images/project_img1fe.jpg" alt="#"/></figure>
+                     <h3>Ferreteria</h3>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="project_box margin_top1 before_bottom">
+                     <figure class="marging_top4"><img  src="images/project_img2canasta.jpg" alt="#"/></figure>
+                     <h3 >Canasta Familiar</h3>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="project_box before_top">
+                     <figure><img src="images/project_img3tec.jpg" alt="#"/></figure>
+                     <h3>Tecnologia&oacute;n</h3>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <a class="read_more" href="#">M&Aacute;S</a>
+               </div>
+            </div>
+         </div>
+      </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="testimonial_box">
+                                          <figure><img src="images/test.png" alt="#"/></figure>
+                                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="testimonial_box">
+                                          <figure><img src="images/test.png" alt="#"/></figure>
+                                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it </p>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                  <i class='fa fa-angle-left'></i>
+                  </a>
+                  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                  <i class='fa fa-angle-right'></i>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+      </div>
+      <!--  footer -->
+   <footer class="bg-dark text-white text-center py-4 mt-5">
+      <p>&copy; 2025 SANEY | Todos los derechos reservados</p>
+   </footer>
+   <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
